8c46370955d7b7d0cccdd83a151d89b96d45f639
dhblight <41066859+dhblight@users.noreply.github.com>
2018-07-20 14:43:38 +0200

    Delete hourminsec.m

--- source/hourminsec.m
+++
@@ -1,9 +0,0 @@
-function[h,m,s] = hourminsec(t)
-
-% gives hours, mins, secs for hh:mm:ss format for a time t given in seconds
-
-temp = t/3600;
-h = floor(temp);
-m = floor((temp-h)*60);
-s = (temp-h-m/60)*3600;
-end
